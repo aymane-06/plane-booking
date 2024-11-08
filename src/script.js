83d76ac7aa4592d8ff03353a2e6a5ba5bd11f087
aymane-06 <aymane.himame@gmail.Com>
2024-11-08 08:46:03 +0100

    release/ticket-reservation

--- src/script.js
+++ src/script.js
@@ -24,7 +24,102 @@ for(let i=0;i<4;i++){
     display.classList.add('hidden');
     display.classList.remove('grid');
     selectFlight.classList.add('hidden');
-    lainding.classList.add('hidden');
+    // lainding.classList.add('hidden');
 })
 }
 }
+let finalP = 0 ;
+    let CNum = 0;
+    let ANum=0;
+    let placeNumber=0;
+function ticketsNum(){
+    const Aticket=document.getElementById('ANum');
+    const Cticket=document.getElementById('CNum');
+    const Aup=document.getElementById('APriceUP');
+    const Adown=document.getElementById('APriceDown');
+    const Cup=document.getElementById('CPriceUp');
+    const Cdown=document.getElementById('CPricedown');
+    const finalPrice=document.getElementById('finalPrice');
+    
+    Aup.addEventListener('click',()=>{
+        ANum++;
+        Aticket.textContent=`${ANum}`;
+        finalP=(CNum*50)+(ANum*104);
+        finalPrice.textContent=`${finalP}$`;
+        placeNumber=ANum+CNum;
+        checkbox();
+        
+
+    })
+    Adown.addEventListener('click',()=>{
+        ANum--;
+        Aticket.textContent=`${ANum}`;
+        if(ANum<=0){
+            ANum=0;
+            Aticket.textContent=`${ANum}`; 
+            
+        }
+        finalP=(CNum*50)+(ANum*104);
+        finalPrice.textContent=`${finalP}$`;
+        placeNumber=ANum+CNum;
+        checkbox();
+        for(let i=0;i<checkboxes.length;i++){
+            checkboxes[i].checked = false;
+        }
+
+    })
+    Cup.addEventListener('click',()=>{
+        CNum++;
+        Cticket.textContent=`${CNum}`
+        finalP=(CNum*50)+(ANum*104);
+        finalPrice.textContent=`${finalP}$`;
+        placeNumber=ANum+CNum;
+        checkbox();
+        
+
+    })
+    Cdown.addEventListener('click',()=>{
+        CNum--;
+        Cticket.textContent=`${CNum}`;
+        if(CNum<=0){
+            CNum=0;
+            Cticket.textContent=`${CNum}`; 
+        }
+        finalP=(CNum*50)+(ANum*104);
+        finalPrice.textContent=`${finalP}$`;
+        placeNumber=ANum+CNum; 
+        checkbox();
+        for(let i=0;i<checkboxes.length;i++){
+            checkboxes[i].checked = false;
+        }
+    })
+    finalPrice.textContent=`0$`;
+    
+  
+}
+const checkboxes = Array.from(document.querySelectorAll('input[type="checkbox"]'));
+for(let i=0;i<checkboxes.length;i++){
+    checkboxes[i].setAttribute('disabled', true);
+}
+
+ticketsNum();
+function checkbox() {
+    
+    let countChecked = 0;
+  
+    checkboxes.forEach(checkbox => {
+      checkbox.removeAttribute('disabled');
+      checkbox.addEventListener('change', () => {
+        countChecked = checkboxes.filter(checkbox => checkbox.checked).length;
+  
+        checkboxes.forEach(checkbox => {
+          if (placeNumber > countChecked) {
+            checkbox.removeAttribute('disabled');
+          } else if (!checkbox.checked) {
+            checkbox.setAttribute('disabled', true);
+          }
+        });
+      });
+    });
+  }
+  
